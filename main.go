511d4e1c8f5d42b9545744fc05a0c1a9940d4d7a
Olga Ivanova <olga.iva@gmail.com>
2022-09-06 17:47:33 +0200

    update conditional

--- main.go
+++ main.go
@@ -14,36 +14,46 @@ func main() {
     fmt.Printf("Welcome to %v booking application\n", conferenceName)
     fmt.Printf("We have total of %v tickets, and %v are still available\n", conferenceTickets, remainingTickets)
 
-   for {
-   var firstName string
-   var lastName string
-   var email string
-   var userTickets uint
+    for {
+    var firstName string
+    var lastName string
+    var email string
+    var userTickets uint
+ 
+    fmt.Println("Enter your first name:")
+    fmt.Scan(&firstName)
+ 
+    fmt.Println("Enter your last name:")
+    fmt.Scan(&lastName)
+ 
+    fmt.Println("Enter your email address:")
+    fmt.Scan(&email)
+ 
+    fmt.Println("Enter your number of tickets:")
+    fmt.Scan(&userTickets)
 
-   fmt.Println("Enter your first name:")
-   fmt.Scan(&firstName)
-
-   fmt.Println("Enter your last name:")
-   fmt.Scan(&lastName)
-
-   fmt.Println("Enter your email address:")
-   fmt.Scan(&email)
-
-   fmt.Println("Enter ynumber of tickets:")
-   fmt.Scan(&userTickets)
-
-   remainingTickets = remainingTickets - userTickets
-   bookings = append(bookings, firstName + " " + lastName)
-
-   firstNames := []string{} 
-   for _, booking := range bookings {
-     var names = strings.Fields(booking) //splits strings with a white space as a separator and returns a slice of strings
-     firstNames = append(firstNames, names[0])
-   }
-
-   fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName,lastName, userTickets, email)
-   fmt.Printf("The remaining number of tickets for the %v is %v.\n", conferenceName, remainingTickets) 
-   fmt.Printf("These are the first names of the bookings: %v.\n", firstNames)
-  }
+    if userTickets <= remainingTickets {
+      remainingTickets = remainingTickets - userTickets
+      bookings = append(bookings, firstName + " " + lastName)
    
+      firstNames := []string{} 
+      for _, booking := range bookings {
+        var names = strings.Fields(booking) //splits strings with a white space as a separator and returns a slice of strings
+        firstNames = append(firstNames, names[0])
+      }
+   
+      fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName,lastName, userTickets, email)
+      fmt.Printf("The remaining number of tickets for the %v is %v.\n", conferenceName, remainingTickets) 
+      fmt.Printf("These are the first names of the bookings: %v.\n", firstNames)
+   
+      if remainingTickets  == 0 {
+       fmt.Println("Our conference is sold out, please return next year")
+       break
+      }
+    } else {
+      fmt.Printf("We only have %v tickets, so you can't book %v tickets.\n", remainingTickets, userTickets)
+    }
+    
+  }
+  
 }
